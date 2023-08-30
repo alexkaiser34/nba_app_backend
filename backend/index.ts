9002bf03f05525967dccfbda120b99cdee77282b
alexkaiser34 <alexkaiser@me.com>
2023-08-30 13:41:57 -0400

    Updated main backend program to run daily updates

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,9 @@
 import cron from 'node-cron';
+import { dailyUpdate } from './db/db_ops';
 
 cron.schedule('0 2,15 * * *', () => {
-    /** TODO: Call daily API updates, updates at 2 AM and 3 PM  */
-  console.log('running a task');
+    /** Call daily API updates, updates at 2 AM and 3 PM  */
+    dailyUpdate()
+    .then(() => console.log('update succeeded!'))
+    .catch((err) => console.log(err));
 });
