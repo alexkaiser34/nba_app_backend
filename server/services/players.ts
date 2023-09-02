0aea9ce8656e8f9cb1043aa25648be8ed12c00b0
alexkaiser34 <alexkaiser@me.com>
2023-09-02 12:48:38 -0400

    Adds teams API route and adds common fuctions to handle requests

--- server/services/players.ts
+++ server/services/players.ts
@@ -3,24 +3,6 @@ import DataBaseActions from "../../backend/db/classes/DataBaseActions";
 import { requestAll, requestByString, requestID } from "../types/request";
 
 
-export async function getAllPlayers(fieldNames: requestAll){
-    const rows = await DataBaseActions.retrieveAll<Player | Player[] | undefined>(
-        'players',
-        fieldNames?.fields
-    );
-    return rows;
-}
-
-
-export async function getPlayerByID(playerID: requestID){
-    const rows = await DataBaseActions.retrieveAllByCondition<Player | Player[] | undefined>(
-        'players',
-        playerID.fields === undefined ? '*' : playerID.fields,
-        `PlayerID=${playerID.id}`
-    );
-    return rows;
-}
-
 export async function getPlayersByTeam(teamID: requestID){
     const rows = await DataBaseActions.retrieveAllByCondition<Player | Player[] | undefined>(
         'players',
@@ -54,19 +36,6 @@ export async function getPlayersByDivision(division: requestByString){
         'INNER',
         "players.TeamID = teams.TeamID",
         `Division='${division.fieldValue}'`
-    );
-    return rows;
-}
-
-/**
- * Let user specify first names and lastnames when making request
- * Allow them to specify AND/OR as well
- */
-export async function getPlayersByName(name: requestByString){
-    const rows = await DataBaseActions.retrieveAllByCondition<Player | Player[] | undefined>(
-        'players',
-        name.fields === undefined ? '*' : name.fields,
-        name.fieldValue
     );
     return rows;
 }
