a84f7749f80d077f65061788ec2ffb2f503bc1ea
alexkaiser34 <alexkaiser@me.com>
2023-09-02 14:50:34 -0400

    Adds scores API route

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,8 @@ import express, { Express, Request, Response } from 'express';
 import { playerRouter } from './routes/players';
 import { standingsRouter } from './routes/standings';
 import { teamRouter } from './routes/teams';
+import { scoresRouter } from './routes/scores';
+
 const PORT = process.env.PORT || '3000';
 
 const app:Express = express();
@@ -21,6 +23,7 @@ app.get("/", (req:Request, res:Response) => {
 app.use("/players", playerRouter);
 app.use("/teams", teamRouter);
 app.use("/standings", standingsRouter);
+app.use("/scores", scoresRouter);
 
 /* Error handler middleware */
 app.use((err, req:Request, res:Response, next) => {
---
+++ server/routes/scores.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import { Game } from '../../backend/api/types/game';
+import { requestByString, requestID } from '../types/request';
+import { getByField, getByID } from '../services/common';
+import { getTeamScores } from '../services/scores';
+
+export const scoresRouter = express.Router();
+
+scoresRouter.get("/", (req, res) => {
+    res.json({ message: "scores ok"});
+});
+
+scoresRouter.get('/getByDay', async function(req, res, next){
+    try {
+        res.json(
+            await getByField<Game>(
+                'games',
+                'Day',
+                req.body as requestByString
+            )
+        );
+    } catch(err) {
+        console.log(`Error while creating player`, err.message);
+        next(err);
+    }
+});
+
+scoresRouter.get('/getByGameID', async function(req, res, next){
+    try {
+        res.json(
+            await getByID<Game>(
+                'games',
+                'GameID',
+                req.body as requestID
+            )
+        );
+    } catch(err) {
+        console.log(`Error while creating player`, err.message);
+        next(err);
+    }
+});
+
+scoresRouter.get('/getByTeam', async function(req, res, next){
+    try {
+        res.json(
+            await getTeamScores(req.body as requestID)
+        );
+    } catch(err) {
+        console.log(`Error while creating player`, err.message);
+        next(err);
+    }
+});
